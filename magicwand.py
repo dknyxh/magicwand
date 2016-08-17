623b0b34c52d43a3adb2812a092da8f56fc9bd25
Kenan Deng <dknyxh@gmail.com>
2016-08-17 14:42:15 -0400

    Add files

---
+++ magicwand.py
@@ -0,0 +1,71 @@
+from scipy import misc
+import numpy as np
+from collections import deque
+
+class Point:
+	def __init__(self,x,y,img,threshold):
+		self.data = img[y][x]
+		self.x = x
+		self.y = y
+		self.width = img.shape[1]
+		self.height = img.shape[0]
+		self.threshold = threshold
+
+	def childs(self):
+		return_list = []
+		if (self.y+1) < self.height:
+			return_list.append((self.x, self.y+1))
+		if (self.y-1) >= 0:
+			return_list.append((self.x, self.y-1))
+		if (self.x-1) >= 0:
+			return_list.append((self.x-1, self.y))
+		if (self.x+1) < self.width:
+			return_list.append((self.x+1, self.y))
+		return return_list
+
+	def inThreshold(self,other_data):
+		if (np.linalg.norm(self.data - other_data)) < self.threshold:
+			return True
+		else:
+			return False
+
+def run(filename, x, y, threshold):
+	input_img = misc.imread(filename)
+	input_img = preprocess_img(input_img)
+
+	output_img = input_img.copy()
+	bit_map = np.zeros(input_img.shape[:2])
+	bit_map.fill(255)
+	queue = deque()
+
+	#first element
+	first = Point(x,y,input_img,threshold)
+	bit_map[y][x] = 0;
+	queue.append(first)
+	# #set bit map
+
+	while len(queue) > 0 :
+		cur = queue.popleft()
+		childs = cur.childs()
+		for (newx,newy) in childs:
+			if (bit_map[newy][newx]) == 255 and first.inThreshold(input_img[newy][newx]):
+				bit_map[newy][newx] = 0;
+				queue.append(Point(newx,newy,input_img,threshold))
+	output_img = np.dstack((output_img, bit_map))
+	misc.imsave(filename.split('.')[0] + "remove.png",output_img)
+
+
+
+def preprocess_img(img):
+	new_img = np.zeros((img.shape[0]+2,img.shape[1]+2,img.shape[2]))
+	new_img.fill(255)
+	new_img[1:img.shape[0]+1,1:img.shape[1]+1] = img
+	return new_img
+
+
+if __name__ =='__main__':
+	import sys
+	if(len(sys.argv)!=5):
+		print("Usage:python magicwand.py [filename] [x] [y] [threshold]")
+	else:
+		run(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
